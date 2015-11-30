030d19a6ad4fa96425bf024d56dd26ea0ff34d11
Nicolò Vescera <slayercandy@icloud.com>
2015-11-30 21:28:22 +0100

    Aggiorna i risultati ogni 5 secondi (tempo da migliorare)

--- script.py
+++ script.py
@@ -3,37 +3,39 @@
 import urllib
 import time
 myfile = open("people","a")
-
-print '-----------|',(time.strftime("%H:%M:%S")),'|-----------'                             
-myfile.write('-----------|'+(time.strftime("%H:%M:%S"))+'|-----------\n')
-
-sock = urllib.urlopen("http://moodlevolta.ictvalleumbra.it/") 
-
-lista = []
 while True:
-	line = sock.readline().replace(' ','')
-	#print line
+	print '-----------|',(time.strftime("%H:%M:%S")),'|-----------'                             
+	myfile.write('-----------|'+(time.strftime("%H:%M:%S"))+'|-----------\n')
+
+	sock = urllib.urlopen("http://moodlevolta.ictvalleumbra.it/") 
+
+	lista = []
+	while True:
+		line = sock.readline().replace(' ','')
+		#print line
 	
-	if line == '':
-		break
-	else:
-		lista.append(line)
-		#print "Aggiunto"
+		if line == '':
+			break
+		else:
+			lista.append(line)
+			#print "Aggiunto"
 
-for line in lista:
-	#print line
-	if line[0:20] == '<liclass="listentry"':
-		inizio = line.rindex('16')+4 #trova l'ultimo 16 e aggiunge 4 posizioni, per rendere il uttto graficamente piu' bello
-		fine = line.rindex('a')-1    #trova l'ultima a e  toglie una posizione, stessa cosa di prima		
+	for line in lista:
+		#print line
+		if line[0:20] == '<liclass="listentry"':
+			inizio = line.rindex('16')+4 #trova l'ultimo 16 e aggiunge 4 posizioni, per rendere il uttto graficamente piu' bello
+			fine = line.rindex('a')-1    #trova l'ultima a e  toglie una posizione, stessa cosa di prima		
 		
-		print line[inizio:fine]
+			print line[inizio:fine]
 
-		myfile.write(line[inizio:fine]+"\n")
+			myfile.write(line[inizio:fine]+"\n")
 
-print "----------------------------------"
-myfile.write("----------------------------------\n")
+	print "----------------------------------"
+	myfile.write("----------------------------------\n")
 
-sock.close()                                        
+	sock.close()
+	time.sleep(5) #5 minuti, ogni quanto moodle aggiorna le persone online
+                                        
 myfile.close()
 #print htmlSource
 
