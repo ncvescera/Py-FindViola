7f6d5bde363f859cc037a7bbba387e388680fc08
Nicolò Vescera <slayercandy@icloud.com>
2015-11-30 21:19:23 +0100

    Aggiunta scrittura su file

--- script.py
+++ script.py
@@ -2,8 +2,10 @@
 
 import urllib
 import time
+myfile = open("people","a")
 
 print '-----------|',(time.strftime("%H:%M:%S")),'|-----------'                             
+myfile.write('-----------|'+(time.strftime("%H:%M:%S"))+'|-----------\n')
 
 sock = urllib.urlopen("http://moodlevolta.ictvalleumbra.it/") 
 
@@ -18,14 +20,20 @@ while True:
 		lista.append(line)
 		#print "Aggiunto"
 
-
 for line in lista:
 	#print line
 	if line[0:20] == '<liclass="listentry"':
 		inizio = line.rindex('16')+4 #trova l'ultimo 16 e aggiunge 4 posizioni, per rendere il uttto graficamente piu' bello
-		fine = line.rindex('a')-1    #trova l'ultima a e  toglie una posizione, stessa cosa di prima
+		fine = line.rindex('a')-1    #trova l'ultima a e  toglie una posizione, stessa cosa di prima		
+		
 		print line[inizio:fine]
+
+		myfile.write(line[inizio:fine]+"\n")
+
 print "----------------------------------"
+myfile.write("----------------------------------\n")
+
 sock.close()                                        
+myfile.close()
 #print htmlSource
 
