e49dc3d0bf41de5124a0aeaceb124e0dc4483369
Nicolò Vescera <slayercandy@icloud.com>
2015-12-10 16:59:09 +0100

    forse risolto bug

--- script/analyzer.py
+++ script/analyzer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 class Persona:
+    dictClassi = ["1A","1B","1C","1D","1E","1F","1G","1H","1L","1M","1N","1P","1Q","1R","1S","1SPGS","1T","2AC","2AGR","2AET","2AINF","2AM","2BC","2BET","2BGR","2BINF","2BM","2CET","2CINF","2CM","2DET","3AC","3BC","4AC","4BC","5AC","3AINF","3BINF","3CINF","4AINF","4BINF","4CINF","5AINF","5BINF","3AGR","3BGR","4AGR","4BGR","5AGR","5BGR","3AT","4AT","5AT","3BT","4BT","5BT","3CT","3AE","4AE","5AE","3BE","4BE","3AM","4AM","5AM","3BM","4BM","5BM","3CM","4CM","3DM","MAGISTER"]
     def __init__(self,nome,classe,cognome):
         self.nome = nome
         self.classe = classe
@@ -13,11 +14,18 @@ class Persona:
 myfile = open("data","r")
 
 nomi = []
+#i = 0
 while True:
-    nomi.append(myfile.readline())
-    if myfile.readline() == '':
+    n = myfile.readline()
+    if n == '':
         break
-        
+
+    nomi.append(n)
+    #print nomi[i]
+    #i = i+1
+    
+myfile.close()       
+
 dizionario = ["1","2","3","4","5"]
 persone = []
 for nome in nomi:
@@ -31,7 +39,7 @@ for nome in nomi:
             alunno = Persona(name,classe,cognome)
             persone.append(alunno)
             #print name+"-"+classe+"-"+cognome
-            #alunno.stampa()
+            alunno.stampa()
         elif nome.find("d.") > 0:
             numero = nome.find("d.")
             name = nome[:numero]
@@ -42,14 +50,15 @@ for nome in nomi:
             persone.append(docente)
             #docente.stampa()
             break
-dictClassi = ["1A","1B","1C","1D","1E","1F","1G","1H","1L","1M","1N","1P","1Q","1R","1S","1SPGS","1T","2AC","2AGR","2AET","2AINF","2AM","2BC","2BET","2BGR","2BINF","2BM","2CET","2CINF","2CM","2DET","3AC","3BC","4AC","4BC","5AC","3AINF","3BINF","3CINF","4AINF","4BINF","4CINF","5AINF","5BINF","3AGR","3BGR","4AGR","4BGR","5BGR","3AT","4AT","5AT","3BT","4BT","5BT","3CT","3AE","4AE","5AE","3BE","4BE","3AM","4AM","5AM","3BM","4BM","5BM","3CM","4CM","3DM","MAGISTER"] #CONTINUARE CON 3 INFORMATICHE IN POI
-for cl in dictClassi:
+
+for cl in Persona.dictClassi:
     qLista = []
     for persona in persone:
+	
         if persona.classe == cl:
             qLista.append(persona)
+	    
     leng = len(qLista)
-    temp = qLista
     if len(qLista) == 0:
         continue
     news = []
@@ -57,6 +66,7 @@ for cl in dictClassi:
     for i in range(1,leng):
         aggiungi = True
         for conf in news:
+	    #conf.stampa()
             if qLista[i].cognome == conf.cognome:
                 aggiungi = False
                 break
@@ -66,3 +76,4 @@ for cl in dictClassi:
     cinf3 = open("../classi/"+cl,"w")   
     for persona in news:
         cinf3.write(persona.stampa())
+    cinf3.close()
