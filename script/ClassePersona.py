269c9c464200582eaf98a9ff607273e79d850ace
Nicolò Vescera <slayercandy@icloud.com>
2015-12-13 20:33:28 +0100

    Classe Persona messa in un file esterno, implementato metodo statico parse in classe

---
+++ script/ClassePersona.py
@@ -0,0 +1,44 @@
+class Persona:
+    dictClassi = ["1A","1B","1C","1D","1E","1F","1G","1H","1L","1M","1N","1P","1Q","1R","1S","1SPGS","1T","2AC","2AGR","2AET","2AINF","2AM","2BC","2BET","2BGR","2BINF","2BM","2CET","2CINF","2CM","2DET","3AC","3BC","4AC","4BC","5AC","3AINF","3BINF","3CINF","4AINF","4BINF","4CINF","5AINF","5BINF","3AGR","3BGR","4AGR","4BGR","5AGR","5BGR","3AT","4AT","5AT","3BT","4BT","5BT","3CT","3AE","4AE","5AE","3BE","4BE","3AM","4AM","5AM","3BM","4BM","5BM","3CM","4CM","3DM","MAGISTER"]
+    def __init__(self,nome,classe,cognome):
+        self.nome = nome
+        self.classe = classe
+        self.cognome = cognome
+    def stampa(self):
+        strPrint = self.nome+" "+self.classe+" "+self.cognome # nome+classe+cognome
+	strReturn = self.nome+" "+self.cognome # nome+cognome
+        print  "[+] ",strPrint
+        return strReturn+"\n"
+    @staticmethod
+    def parse(nome):
+	dizionario = ["1","2","3","4","5"]
+	#print nome[:-1]
+	for confronto in dizionario:
+		if nome.find(confronto)>0:
+			numero = nome.find(confronto)
+	    		name = nome[:numero]
+			classe = nome[numero:nome.find("_")]
+			cognome = nome[nome.find("_")+1:-1]
+		    	#alunno = 
+			#alunno.stampa()
+		    	return Persona(name,classe,cognome)
+		    	#persone.append(alunno)
+		    	#print name+"-"+classe+"-"+cognome
+		    	
+		elif nome.find("d.") > 0:
+		   	numero = nome.find("d.")
+		    	name = nome[:numero]
+		    	classe = "MAGISTER"
+		    	cognome = nome[nome.find(".")+1:-1]
+		    	#print name+"-"+classe+"-"+cognome
+		    	docente = Persona(name,classe,cognome)
+		    	#persone.append(docente)
+		    	return docente
+		    	#docente.stampa()
+		    	break
+		elif nome.find("Utente") > 0:
+			name = "Amministratore"
+			classe = "ADMIN"
+			cognome = "Utente"
+			admin = Persona(name,classe,cognome)
+			return admin
--- script/analyzer.py
+++ script/analyzer.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python
 import operator
-
-class Persona:
-    dictClassi = ["1A","1B","1C","1D","1E","1F","1G","1H","1L","1M","1N","1P","1Q","1R","1S","1SPGS","1T","2AC","2AGR","2AET","2AINF","2AM","2BC","2BET","2BGR","2BINF","2BM","2CET","2CINF","2CM","2DET","3AC","3BC","4AC","4BC","5AC","3AINF","3BINF","3CINF","4AINF","4BINF","4CINF","5AINF","5BINF","3AGR","3BGR","4AGR","4BGR","5AGR","5BGR","3AT","4AT","5AT","3BT","4BT","5BT","3CT","3AE","4AE","5AE","3BE","4BE","3AM","4AM","5AM","3BM","4BM","5BM","3CM","4CM","3DM","MAGISTER"]
-    def __init__(self,nome,classe,cognome):
-        self.nome = nome
-        self.classe = classe
-        self.cognome = cognome
-    def stampa(self):
-        #string = self.nome+" "+self.classe+" "+self.cognome # nome+classe+cognome
-	string = self.nome+" "+self.cognome # nome+cognome
-        print  "[+] ",string
-        return string+"\n"
+from ClassePersona import *
 
 myfile = open("data","r")
 
 nomi = []
-i = 0 #conta il numero di persone totali
+totale = 0 #conta il numero di persone totali
 while True:
     n = myfile.readline()
     if n == '':
@@ -28,30 +17,10 @@ while True:
     
 myfile.close()       
 
-dizionario = ["1","2","3","4","5"]
+
 persone = []
 for nome in nomi:
-    #print nome[:-1]
-    for confronto in dizionario:
-        if nome.find(confronto)>0:
-            numero = nome.find(confronto)
-            name = nome[:numero]
-            classe = nome[numero:nome.find("_")]
-            cognome = nome[nome.find("_")+1:-1]
-            alunno = Persona(name,classe,cognome)
-            persone.append(alunno)
-            #print name+"-"+classe+"-"+cognome
-            #alunno.stampa()
-        elif nome.find("d.") > 0:
-            numero = nome.find("d.")
-            name = nome[:numero]
-            classe = "MAGISTER"
-            cognome = nome[nome.find(".")+1:-1]
-            #print name+"-"+classe+"-"+cognome
-            docente = Persona(name,classe,cognome)
-            persone.append(docente)
-            #docente.stampa()
-            break
+	persone.append((Persona.parse(nome)))
 
 for cl in Persona.dictClassi:
     qLista = []
@@ -79,7 +48,8 @@ for cl in Persona.dictClassi:
     cinf3 = open("../classi/"+cl,"w")   
     for persona in news:
         cinf3.write(persona.stampa())
-	i = i+1
+	totale = totale+1
     cinf3.write(str(len(news))+"\n")
     cinf3.close()
-print "--------------------------------------------\nNumero di persone totali: ",i
+print "--------------------------------------------\nNumero di persone totali: ",totale
+
