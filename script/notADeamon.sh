090d3d64785d5d197fadc519033c296bbbfe4c52
Nicolò Vescera <slayercandy@icloud.com>
2016-01-10 11:54:21 +0100

    Creato Branch Cygwin e adattato gli script per lavorare sotto cygwin

--- script/notADeamon.sh
+++
@@ -1,9 +0,0 @@
-while :
-do
-	psOn=`ps -A |grep catcher.py`
-	if [ !$psOn ]
-		then
-			gnome-terminal -e  ./catcher.py
-	fi
-	sleep 1800
-done
--- start.sh
+++ start.sh
@@ -7,15 +7,16 @@ if [ ! -d ../classi ]
 fi
 ##FineControllo##
 
-./notADeamon.sh & #avvia in background lo script
+./analyzingLoop.sh & #avvia in background lo script
 #gnome-terminal -e 
 #./catcher.py
 while :
 do
-	echo "Waiting ..."
-	sleep 3600	
-	echo "Analizing ..."
-
-	./analyzer.py
-	echo "DONE"
+        psOn=`ps -a |grep python`
+        if [ !$psOn ]
+                then
+                        ./catcher.py
+        fi
+        sleep 1800
 done
+
